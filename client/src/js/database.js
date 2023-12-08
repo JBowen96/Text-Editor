9595789504ae1ecf11d0883fec0759baf4d8aaa1
Josh B <bowenpjoshua@gmail.com>
2023-12-07 22:04:47 -0500

    updated code and bug fixes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,35 +1,46 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// Add text to indexedDB
+// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDB = await openDB('jate', 1);
-  const tx = jateDB.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readwrite");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Pass in content
   const request = store.put({ id: 1, value: content });
+  // Confirmation
   const result = await request;
-  console.log(result);
+  console.log("🚀 - data saved to the database", result);
 };
 
-// Retrieve text from indexedDB
-export const getDb = async (e) => {
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readonly");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Get all request
+  const request = store.getAll();
+  // Confirmation and return
   const result = await request;
-  return result?.value;
+  console.log("🚀 - data read from database", result);
+  return result.value;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,67 +1,74 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-// * TODO: Add and configure workbox plugins for a service worker and manifest file.
-// * Add CSS loaders and babel to webpack.
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+// TODO: Configure workbox plugins for a service worker and manifest file.
+const WorkboxPlugin = require("workbox-webpack-plugin");
+// TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
-        name: 'JATE',
-        short_name: 'JATE',
-        description: 'Just Another Text Editor',
-        display: 'standalone',
-        background_color: '#1e1e1e',
-        theme_color: '#1e1e1e',
-        start_url: '/',
-        publicPath: '/',
         fingerprints: false,
         inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "offline text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'service-worker.js',
-      }),
     ],
-
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
               plugins: [
-                '@babel/plugin-proposal-object-rest-spread',
-                '@babel/transform-runtime',
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
               ],
             },
           },
